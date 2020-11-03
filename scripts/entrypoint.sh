fd7b94e893f1896c00e3ea6f0ae1b8c83913061d
B0go <victorbogo@icloud.com>
2020-11-03 12:05:07 -0300

    Add entrypoint used to curl data to newrelic
    Signed-off-by: B0go <victorbogo@icloud.com>

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+json=$( jq -n \
+                --arg et "$EVENT_TYPE" \
+                --arg st "$DEPLOYMENT_STATUS" \
+                --arg dc "$DESCRIPTION" \
+                --arg ac "$ACTOR" \
+                --arg pj "$PROJECT" \
+                '{eventType: $et, status: $st, description: $dc, actor: $ac, project: $pj }' )
+
+echo "$json" | gzip -c | curl -X POST \
+    -H "Content-Type: application/json" \
+    -H "X-Insert-Key: $NEW_RELIC_INGIGHTS_INSERT_KEY" \
+    -H "Content-Encoding: gzip" \
+    --data-binary @- \
+    "$NEWRELIC_URL"
